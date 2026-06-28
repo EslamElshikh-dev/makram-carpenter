d07c9b936d0262aea2b23f24bcd57927970d483d
Eslam Eshikh <moqawel1215@gmail.com>
2026-06-29 00:33:25 +0300

    Revert "Build Arabic RTL multi-page services site for Makram"

--- assets/js/main.js
+++
@@ -1,1 +0,0 @@
-document.querySelector('.nav-toggle')?.addEventListener('click',()=>document.querySelector('.nav')?.classList.toggle('open'));document.querySelectorAll('.nav a').forEach(a=>a.addEventListener('click',()=>document.querySelector('.nav')?.classList.remove('open')));
